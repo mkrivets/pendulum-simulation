740fa27fb7feef40adf9e0cfda743393c2cb6e97
mkrivets <mkrivets1@gmail.com>
2020-11-30 06:49:46 +0200

    finished animation of Euler-Cauchy

--- Euler-Cauchy.py
+++ Euler-Cauchy.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+plt.style.use('ggplot')
 
 v = 0.2  # frequency
-a = 0.3  # amplitude
+a = 0.1  # amplitude
 g = 9.8  # free fall acceleration
 length = 20  # length of rigid and light pendulum
+tMax = 250  # the number of seconds
 
 t0 = 0  # moment
-h = 0.01  # step
+h = 0.2  # step
 y0 = np.pi / 3  # angle
-u0 = 1  # accelerate
+u0 = 0.75  # accelerate
 
 approx_y_list = []
 accurate_y_list = []
@@ -28,7 +31,7 @@ def derivative_func(t, f):
 
 
 t = 0
-while (t < 100):
+while (t < tMax):
     t_list.append(t)
     t += h
 
@@ -46,4 +49,37 @@ while (i < len(t_list)):
 print(accurate_y_list)
 plt.plot(t_list, accurate_y_list)
 plt.savefig('Euler-Cauchy.png')
+plt.show()
+
+# animation
+
+x_horizontal = []
+w_vertical = []
+for y in accurate_y_list:
+    x_horizontal.append(length * np.sin(y))
+    w_vertical.append(length * np.cos(y))
+print(x_horizontal)
+print(w_vertical)
+
+# Scatter plot
+fig = plt.figure()
+axes = fig.add_subplot()
+axes.set_xlim(min(x_horizontal)-1, max(x_horizontal)+1)
+axes.set_ylim(min(w_vertical)-1, max(w_vertical)+1)
+plt.axis('square')
+
+point, = axes.plot([x_horizontal[0]],[w_vertical[0]], 'go')
+line, = axes.plot([x_horizontal[0],0],[w_vertical[0],0])
+
+def ani(coords):
+    point.set_data([coords[0]],[coords[1]])
+    line.set_data([coords[0], 0],[coords[1], 0])
+    return point, line
+
+def frames():
+    for x_pos, w_pos in zip(x_horizontal, w_vertical):
+        yield x_pos, w_pos
+
+ani = FuncAnimation(fig, ani, frames=frames, interval = 50)
+ani.save("Euler-Cauchy.mp4")
 plt.show()
